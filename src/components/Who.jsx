6a0a63d39a94306afeff3ad52d62df9796c9e092
Yurii Rybachok <drummer2009@ukr.net>
2023-03-06 09:37:03 +0200

    snap scroll effect

--- src/components/Who.jsx
+++ src/components/Who.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const Section = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+`;
 
 const Who = () => {
   return (
-    <div>Who</div>
+    <Section>Who</Section>
   )
 }
 
