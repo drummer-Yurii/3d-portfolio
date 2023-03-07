077b6add0ae563e18eac111055dcbf852c71e078
Yurii Rybachok <drummer2009@ukr.net>
2023-03-07 10:42:55 +0200

    add components to works component & listen click

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import Development from './Development';
+import ProductDesign from './ProductDesign';
+import WebDesign from './WebDesign';
 
 const data = ['Web Design', 'Development', 'Illustration', 'Product Design', 'Social Media'];
 
@@ -34,7 +37,7 @@ const ListItem = styled.li`
   position: relative;
 
   ::after {
-    content: "${(props) => props.text}";
+    content: '${(props) => props.text}';
     position: absolute;
     top: 0;
     left: 0;
@@ -48,7 +51,7 @@ const ListItem = styled.li`
       animation: moveText 0.5s linear both;
 
       @keyframes moveText {
-        to{
+        to {
           width: 100%;
         }
       }
@@ -60,17 +63,28 @@ const Right = styled.div`
 `;
 
 const Works = () => {
+  const [work, setWork] = useState('Web Design');
   return (
     <Section>
       <Container>
         <Left>
           <List>
             {data.map((item) => (
-              <ListItem key={item} text={item}>{item}</ListItem>
+              <ListItem key={item} text={item} onClick={() => setWork(item)}>
+                {item}
+              </ListItem>
             ))}
           </List>
         </Left>
-        <Right></Right>
+        <Right>
+          {work === 'Web Design' ? (
+            <WebDesign />
+          ) : work === 'Development' ? (
+            <Development />
+          ) : (
+            <ProductDesign />
+          )}
+        </Right>
       </Container>
     </Section>
   );
