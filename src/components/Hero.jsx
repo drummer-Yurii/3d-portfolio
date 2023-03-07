3c7e463617cc7b24d73f3388e033ed7e16589adb
Yurii Rybachok <drummer2009@ukr.net>
2023-03-07 10:24:53 +0200

    add threejs effect to herro component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import { OrbitControls, MeshDistortMaterial, Sphere } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import React from 'react';
 import styled from 'styled-components';
 import Navbar from './Navbar';
 
@@ -69,8 +71,8 @@ const Img = styled.img`
   animation: animate 2s infinite ease alternate;
 
   @keyframes animate {
-    to{
-        transform: translateY(20px);
+    to {
+      transform: translateY(20px);
     }
   }
 `;
@@ -78,24 +80,31 @@ const Img = styled.img`
 const Hero = () => {
   return (
     <Section>
-        <Navbar />
-        <Container>
-            <Left>
-                <Title>Think. Make. Solve.</Title>
-                <WhatWeDo>
-                    <Line src="./img/line.png" />
-                    <Subtitle>What we Do</Subtitle>
-                </WhatWeDo>
-                <Desc>we enjoi creating delightful, human-centered digital experiences.</Desc>
-                <Button>Learn More</Button>
-            </Left>
-            <Right>
-                {/* 3d model */}
-                <Img src="./img/moon.png" />
-            </Right>
-        </Container>
+      <Navbar />
+      <Container>
+        <Left>
+          <Title>Think. Make. Solve.</Title>
+          <WhatWeDo>
+            <Line src="./img/line.png" />
+            <Subtitle>What we Do</Subtitle>
+          </WhatWeDo>
+          <Desc>we enjoi creating delightful, human-centered digital experiences.</Desc>
+          <Button>Learn More</Button>
+        </Left>
+        <Right>
+          <Canvas>
+            <OrbitControls enableZoom={false} />
+            <ambientLight intensity={1} />
+            <directionalLight position={[3, 2, 1]} />
+            <Sphere args={[1, 100, 200]} scale={3}>
+              <MeshDistortMaterial color="#3d1c56" attach="material" distort={0.5} speed={2} />
+            </Sphere>
+          </Canvas>
+          <Img src="./img/moon.png" />
+        </Right>
+      </Container>
     </Section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
