f7c02d06d49d8e8bc72cb76b526b72bcc2a6af3b
Yurii Rybachok <drummer2009@ukr.net>
2023-03-07 10:12:21 +0200

    add threejs cube to who component

--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -7,12 +7,12 @@ const Cube = () => {
     useFrame((state) => (textRef.current.position.x = Math.sin(state.clock.elapsedTime) * 2));
   return (
     <mesh>
-      <boxGeometry args={[2, 2, 2]} />
+      <boxGeometry />
       <meshStandardMaterial>
         <RenderTexture attach="map">
-          <PerspectiveCamera makeDefault position={[0, 0, 2]} />
+          <PerspectiveCamera makeDefault position={[0, 0, 5]} />
           <color attach="background" args={['#dc9dcd']} />
-          <Text ref={textRef} fontSize={1} color="#555">
+          <Text ref={textRef} fontSize={3} color="#555">
             hello
           </Text>
         </RenderTexture>
